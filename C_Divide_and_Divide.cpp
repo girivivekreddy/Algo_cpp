f7081f1e0191d07dece121ceaf68dde35e326de5
Vivek Reddy Giri <127582990+girivivekreddy@users.noreply.github.com>
2024-02-10 18:44:23 +0530

    AC340

--- C_Divide_and_Divide.cpp
+++ C_Divide_and_Divide.cpp
@@ -1,25 +1,34 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 
-
-long long ans(long long num){
-    if(num<2){
+unordered_map<long long, long long> memo;
+long long ans(long long num) {
+    if (num < 2) {
         return 0;
-    }long long a;
-    if((num%2) ==0 ){
-        a = num/2;
-    }else{
-        a = num/2 +1;
     }
-    return num+ans((num/2))+ans(a);
+    if (memo.find(num) != memo.end()) {
+        return memo[num];
+    }
+    long long a;
+    if (num % 2 == 0) {
+        a = num / 2;
+    } else {
+        a = num / 2 + 1;
+    }
+    long long result = num + ans(num / 2) + ans(a);
+    memo[num] = result;
+    return result;
 }
 
-int main(){
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    long long num,sum=0;
-    cin>>num;
-    
-    cout<<sum<<endl;
+
+    long long num, sum;
+    cin >> num;
+    sum = ans(num);
+    cout << sum << endl;
+
     return 0;
 }
