f95c9640e5af3fcab8e77da56bb5dc84dc67014f
girivivekreddy <girivivekreddy@gmail.com>
2024-05-17 17:02:14 +0530

    Stresstest

--- CDSA/Lang/2_1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int gallons;
-    cout<<"Enter number of gallons: ";
-    cin>>gallons;
-    cout<<gallons<<" gallons are "<<gallons/7.481<<" cubic feet"<<endl;
-    return 0;
-}
